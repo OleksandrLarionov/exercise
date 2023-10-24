4f24080cdf2ecfb27cfc139167a509dc89fddde7
OleksandrLarionov <larionov.alex23@gmail.com>
2023-10-24 17:26:16 +0200

    cards style

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -15,10 +15,10 @@ const AllTheBooks = () => {
 			<Row>
 				{horror.map((book) => {
 					return (
-						<Col md={2} key={book.asin}>
-							<Card>
-								<Card.Img variant='top' src={book.img} />
-								<Card.Body>
+						<Col md={3} key={book.asin}>
+							<Card className='h-100'>
+								<Card.Img variant='top' src={book.img} height={400} />
+								<Card.Body className='justify-content-between d-flex flex-column'>
 									<Card.Title>{book.title}</Card.Title>
 									<Card.Text>{book.category}</Card.Text>
 									<Button variant='primary'>Buy</Button>
